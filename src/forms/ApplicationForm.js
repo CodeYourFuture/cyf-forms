e2aec13d53531b45fb202403b43c9f46f9c144c2
Kadir <kegilmez@gmail.com>
2019-06-19 17:56:20 +0100

    cities options modified

--- src/forms/ApplicationForm.js
+++ src/forms/ApplicationForm.js
@@ -1,88 +1,103 @@
-import React, { lazy, Suspense, Component } from 'react';
-import PropTypes from 'prop-types';
-import LoadingAnimation from '../components/LoadingAnimation';
-const ApplicationFormGeneral = lazy(() => import('./ApplicationFormGeneral'));
-const ApplicationFormCode = lazy(() => import('./ApplicationFormCode'));
-const ApplicationFormOps = lazy(() => import('./ApplicationFormOps'));
-const ApplicationFormThanks = lazy(() => import('./ApplicationFormThanks'));
+import React, { lazy, Suspense, Component } from "react";
+import PropTypes from "prop-types";
+import LoadingAnimation from "../components/LoadingAnimation";
+import { getCities } from "../actions";
+const ApplicationFormGeneral = lazy(() => import("./ApplicationFormGeneral"));
+const ApplicationFormCode = lazy(() => import("./ApplicationFormCode"));
+const ApplicationFormOps = lazy(() => import("./ApplicationFormOps"));
+const ApplicationFormThanks = lazy(() => import("./ApplicationFormThanks"));
 
 class ApplicationForm extends Component {
-    constructor(props) {
-        super(props);
-        this.nextPage = this.nextPage.bind(this);
-        this.previousPage = this.previousPage.bind(this);
-        this.state = {
-            page: 1
-        };
-    }
+  state = {
+    page: 1,
+    cities: []
+  };
 
-    nextPage() {
-        this.setState({ page: this.state.page + 1 });
-    }
+  componentWillMount = async () => {
+    const cities = await getCities();
+    this.setState({ cities: cities });
+  };
 
-    previousPage() {
-        this.setState({ page: this.state.page - 1 });
-    }
+  nextPage = () => {
+    this.setState({ page: this.state.page + 1 });
+  };
 
-    render() {
-        const { onSubmit, values, hasSubmissionSucceeded } = this.props;
-        const { page } = this.state;
-        const stage = hasSubmissionSucceeded === true ? 4 : page;
+  previousPage = () => {
+    this.setState({ page: this.state.page - 1 });
+  };
 
-        const isInterestedInCode = values.fieldsOfInterest && values.fieldsOfInterest['Teaching code or agile methodologies'] === true;
-        const isInterestedInOps = values.fieldsOfInterest && values.fieldsOfInterest['Running and growing the organisation'] === true;
+  render() {
+    const { onSubmit, values, hasSubmissionSucceeded } = this.props;
+    const { page, cities } = this.state;
 
-        const fallbackElement = <LoadingAnimation isVisible={true} />;
+    const stage = hasSubmissionSucceeded === true ? 4 : page;
+    const isInterestedInCode =
+      values.fieldsOfInterest &&
+      values.fieldsOfInterest["Teaching code or agile methodologies"] === true;
+    const isInterestedInOps =
+      values.fieldsOfInterest &&
+      values.fieldsOfInterest["Running and growing the organisation"] === true;
 
-        const stages = {
-            1: <Suspense fallback={fallbackElement}>
-                <ApplicationFormGeneral onSubmit={this.nextPage} />
-              </Suspense>,
-            2: isInterestedInCode
-                ? <Suspense fallback={fallbackElement}>
-                    <ApplicationFormCode
-                      onSubmit={isInterestedInOps ? this.nextPage : onSubmit}
-                      previousPage={this.previousPage}
-                      nextButtonLabel={isInterestedInOps ? "Next" : "Submit"}
-                    />
-                  </Suspense>
-                : <Suspense fallback={fallbackElement}>
-                    <ApplicationFormOps
-                      onSubmit={onSubmit}
-                      previousPage={this.previousPage}
-                      nextButtonLabel="Submit"
-                    />
-                  </Suspense>,
-            3: isInterestedInCode && isInterestedInOps
-                ? <Suspense fallback={fallbackElement}>
-                    <ApplicationFormOps
-                      onSubmit={onSubmit}
-                      previousPage={this.previousPage}
-                      nextButtonLabel="Submit"
-                     />
-                  </Suspense>
-                : <Suspense fallback={fallbackElement}>
-                    <ApplicationFormThanks />
-                  </Suspense>,
-            4: <Suspense fallback={fallbackElement}>
-                 <ApplicationFormThanks />
-              </Suspense>,
-        };
+    const fallbackElement = <LoadingAnimation isVisible={true} />;
 
-        return (
-            <div>
-                <h1>CYF Volunteer Application Form</h1>
-                {stages[stage]}
-            </div>
-        )
-    }
+    const stages = {
+      1: (
+        <Suspense fallback={fallbackElement}>
+          <ApplicationFormGeneral onSubmit={this.nextPage} cities={cities} />
+        </Suspense>
+      ),
+      2: isInterestedInCode ? (
+        <Suspense fallback={fallbackElement}>
+          <ApplicationFormCode
+            onSubmit={isInterestedInOps ? this.nextPage : onSubmit}
+            previousPage={this.previousPage}
+            nextButtonLabel={isInterestedInOps ? "Next" : "Submit"}
+          />
+        </Suspense>
+      ) : (
+        <Suspense fallback={fallbackElement}>
+          <ApplicationFormOps
+            onSubmit={onSubmit}
+            previousPage={this.previousPage}
+            nextButtonLabel="Submit"
+          />
+        </Suspense>
+      ),
+      3:
+        isInterestedInCode && isInterestedInOps ? (
+          <Suspense fallback={fallbackElement}>
+            <ApplicationFormOps
+              onSubmit={onSubmit}
+              previousPage={this.previousPage}
+              nextButtonLabel="Submit"
+            />
+          </Suspense>
+        ) : (
+          <Suspense fallback={fallbackElement}>
+            <ApplicationFormThanks />
+          </Suspense>
+        ),
+      4: (
+        <Suspense fallback={fallbackElement}>
+          <ApplicationFormThanks />
+        </Suspense>
+      )
+    };
+
+    return (
+      <div>
+        <h1>CYF Volunteer Application Form</h1>
+        {stages[stage]}
+      </div>
+    );
+  }
 }
 
 ApplicationForm.propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-    values: PropTypes.object.isRequired,
-    isSubmissionInProgress: PropTypes.bool.isRequired,
-    hasSubmissionSucceeded: PropTypes.bool.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  values: PropTypes.object.isRequired,
+  isSubmissionInProgress: PropTypes.bool.isRequired,
+  hasSubmissionSucceeded: PropTypes.bool.isRequired
 };
 
 export default ApplicationForm;
--- src/forms/ApplicationFormGeneral.js
+++ src/forms/ApplicationFormGeneral.js
@@ -1,74 +1,87 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { reduxForm } from 'redux-form';
+import React from "react";
+import PropTypes from "prop-types";
+import { reduxForm } from "redux-form";
 
-import ShortAnswerQuestion from '../components/ShortAnswerQuestion';
-import EmailQuestion from '../components/EmailQuestion';
-import CheckboxesQuestion from '../components/MultipleAnswerQuestion';
-import DropdownQuestion from '../components/DropdownQuestion';
-import Button from '../components/Button';
+import ShortAnswerQuestion from "../components/ShortAnswerQuestion";
+import EmailQuestion from "../components/EmailQuestion";
+import CheckboxesQuestion from "../components/MultipleAnswerQuestion";
+import DropdownQuestion from "../components/DropdownQuestion";
+import Button from "../components/Button";
 import ButtonGroup from "../components/ButtonGroup";
 
-import * as validators from '../utils/validators';
+import * as validators from "../utils/validators";
 
-const ApplicationFormGeneral = ({ handleSubmit, invalid }) => (
-    <form autoComplete="off" className="applicationForm" onSubmit={handleSubmit}>
-        <p>So, you're interested in volunteering for CodeYourFuture?
-            Fantastic! We just need a few pieces of information to help us get organised and welcome you.
-        </p>
-        <ShortAnswerQuestion title="What's your first name?" name="firstName" isRequired={true} />
-        <ShortAnswerQuestion title="And what's your last name?" name="lastName" />
-        <DropdownQuestion
-            title="Which CYF city would you like to volunteer for?"
-            description="If you're interested in bringing CYF to your city email us at contact@codeyourfuture.io."
-            name="city"
-            options={[
-                'Cape Town (South Africa)',
-                'London (UK)',
-                'Manchester (UK)',
-                'Glasgow (UK)',
-                'Rome (Italy)',
-                'Medellín (Colombia)',
-            ].sort()}
-            isRequired={true}
-        />
-        <EmailQuestion title="What's your email address?" name="email" isRequired={true} />
-        <ShortAnswerQuestion
-            title="What's your mobile number?"
-            description="If you don't mind us contacting you this way"
-            name="phone"
-        />
-        <CheckboxesQuestion
-            title="Cool. What are you interested in helping with?"
-            name="fieldsOfInterest"
-            options={[
-                'Teaching code or agile methodologies',
-                'Running and growing the organisation',
-            ]}
-            isRequired={true}
-        />
-        <ButtonGroup>
-            <Button title="Next" type="submit" disabled={invalid} />
-        </ButtonGroup>
-    </form>
+const ApplicationFormGeneral = ({ handleSubmit, invalid, cities }) => (
+  <form autoComplete="off" className="applicationForm" onSubmit={handleSubmit}>
+    <p>
+      So, you're interested in volunteering for CodeYourFuture? Fantastic! We
+      just need a few pieces of information to help us get organised and welcome
+      you.
+    </p>
+    <ShortAnswerQuestion
+      title="What's your first name?"
+      name="firstName"
+      isRequired={true}
+    />
+    <ShortAnswerQuestion title="And what's your last name?" name="lastName" />
+    <DropdownQuestion
+      title="Which CYF city would you like to volunteer for?"
+      description="If you're interested in bringing CYF to your city email us at contact@codeyourfuture.io."
+      name="city"
+      options={cities}
+      // {[
+      //     'Cape Town (South Africa)',
+      //     'London (UK)',
+      //     'Manchester (UK)',
+      //     'Glasgow (UK)',
+      //     'Rome (Italy)',
+      //     'Medellín (Colombia)',
+      // ].sort()}
+      isRequired={true}
+    />
+    <EmailQuestion
+      title="What's your email address?"
+      name="email"
+      isRequired={true}
+    />
+    <ShortAnswerQuestion
+      title="What's your mobile number?"
+      description="If you don't mind us contacting you this way"
+      name="phone"
+    />
+    <CheckboxesQuestion
+      title="Cool. What are you interested in helping with?"
+      name="fieldsOfInterest"
+      options={[
+        "Teaching code or agile methodologies",
+        "Running and growing the organisation"
+      ]}
+      isRequired={true}
+    />
+    <ButtonGroup>
+      <Button title="Next" type="submit" disabled={invalid} />
+    </ButtonGroup>
+  </form>
 );
 
 ApplicationFormGeneral.propTypes = {
-    onSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
 };
 
 const validate = values => ({
-    firstName: validators.required(values.firstName),
-    email: validators.required(values.email),
-    city: validators.required(values.city),
-    fieldsOfInterest: {
-        'Teaching code or agile methodologies': validators.required(values.fieldsOfInterest),
-    },
+  firstName: validators.required(values.firstName),
+  email: validators.required(values.email),
+  city: validators.required(values.city),
+  fieldsOfInterest: {
+    "Teaching code or agile methodologies": validators.required(
+      values.fieldsOfInterest
+    )
+  }
 });
 
 export default reduxForm({
-    form: 'applicationForm',
-    destroyOnUnmount: false,
-    forceUnregisterOnUnmount: true,
-    validate,
+  form: "applicationForm",
+  destroyOnUnmount: false,
+  forceUnregisterOnUnmount: true,
+  validate
 })(ApplicationFormGeneral);
