be89414faf5cd41ffd382c29ac81a69ed99208dd
nabraha <nebay.tedros@yahoo.com>
2019-12-20 16:13:26 +0000

    Change last name placeholder/#140

--- src/Components/forms/inputs/index.js
+++ src/Components/forms/inputs/index.js
@@ -48,7 +48,7 @@ export default class VolunteerForm extends Component {
             isEmpty={errors.lastName}
             label="Last Name *"
             name="lastName"
-            placeholder="Your first name..."
+            placeholder="Your last name..."
             type="text"
           />
           <TextInput
