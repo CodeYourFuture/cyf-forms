d9664225f64258c3bb940ffeda3248cb324a41f0
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-10-08 21:47:58 +0100

    Migrate to Testing Library selectors
    And fix issue with accessibility of phone number input.

--- e2e/support/commands.js
+++ e2e/support/commands.js
@@ -3,62 +3,34 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************//
+import '@testing-library/cypress/add-commands'
 
 Cypress.Commands.add('fillInitialForm', data => {
   // mandatory fields
-  cy.get('[name="firstName"]').type(data.firstName)
-  cy.get('[name="lastName"]').type(data.lastName)
-  cy.get('[name="cityId"]').select(data.cityName)
-  cy.get('[name="email"]').type(data.email)
-  cy.get('input[type="tel"]').type(data.tel)
-  cy.get('[name="interestedInVolunteer"]').type(data.interestedInVolunteer)
-  cy.get('[name="interestedInCYF"]').type(data.interestedInCYF)
+  cy.findByRole('textbox', { name: /first name/i }).type(data.firstName)
+  cy.findByRole('textbox', { name: /last name/i }).type(data.lastName)
+  cy.findByRole('combobox', { name: /city/i }).select(data.cityName)
+  cy.findByRole('textbox', { name: /email/i }).type(data.email)
+  cy.findByRole('textbox', { name: /phone number/i }).type(data.tel)
+  cy.findByRole('textbox', { name: /interested in volunteering/i }).type(
+    data.interestedInVolunteer
+  )
+  cy.findByRole('textbox', { name: /interested in code your future/i }).type(
+    data.interestedInCYF
+  )
 
   // optional fields
   if (data.industry) {
-    cy.get('[name="industry"]').select(data.industry)
+    cy.findByRole('combobox', { name: /industry/i }).select(data.industry)
   }
   if (data.hearAboutCYF) {
-    cy.get('[name="hearAboutCYF"]').select(data.hearAboutCYF)
+    cy.findByRole('combobox', { name: /hear about code your future/i }).select(
+      data.hearAboutCYF
+    )
   }
 
   // submission
-  cy.get('[name="agreeToTOU"]').check()
-  cy.get('[name="agreeToReceiveCommunication"]').check()
-  cy.get('button')
-    .contains('Submit')
-    .click()
-})
-
-Cypress.Commands.add('fillCodeForm', (data, button) => {
-  Object.keys(data.codeExpertise).forEach(area => {
-    const level = parseInt(data.codeExpertise[area])
-    cy.get(`[name="codeExpertise['${area}']"]`)
-      .eq(level - 1)
-      .check()
-  })
-  if (data.otherCodeExpertise) {
-    cy.get('[name="otherCodeExpertise"]').type(data.otherCodeExpertise)
-  }
-  if (data.availableOnWeekends) {
-    const index = ['Yes', 'No'].indexOf(data.availableOnWeekends)
-    cy.get('[name="availableOnWeekends"]')
-      .eq(index)
-      .check()
-  }
-  cy.get('button')
-    .contains(button)
-    .click()
-})
-
-Cypress.Commands.add('fillOrgForm', (data, button) => {
-  Object.keys(data.skillSets).forEach(skill => {
-    cy.get(`[name="skillSets[${skill}]"]`).check()
-  })
-  Object.keys(data.availability).forEach(slot => {
-    cy.get(`[name="availability[${slot}]"]`).check()
-  })
-  cy.get('button')
-    .contains(button)
-    .click()
+  cy.findByRole('checkbox', { name: /terms of use/i }).check()
+  cy.findByRole('checkbox', { name: /contact me/i }).check()
+  cy.findByRole('button', { name: /submit/i }).click()
 })
