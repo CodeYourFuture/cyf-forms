65f27aa298fedbdd2b7c2af15349054f8f594e58
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-10-10 11:02:54 +0100

    Prepare for React Router v6 upgrade
    Guidance: https://reactrouter.com/en/main/upgrading/v5

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Public from '../Layout/Public'
+import { Route, Switch, useLocation, useParams } from 'react-router-dom'
+
 import Forms from '../Components/forms'
 import NotFound from '../Components/NotFound'
 
+function WithRouteProps({ children: Component }) {
+  const location = useLocation()
+  const params = useParams()
+  return <Component location={location} match={{ params }} />
+}
+
 export default () => (
   <Switch>
-    <Public exact path="/" component={Forms} />
-    <Public exact path="/code/:userId/:code?" component={Forms} />
-    <Route path="*" component={NotFound} />
+    <Route exact path="/">
+      <WithRouteProps children={Forms} />
+    </Route>
+    <Route exact path="/code/:userId/:code?">
+      <WithRouteProps children={Forms} />
+    </Route>
+    <Route path="*">
+      <NotFound />
+    </Route>
   </Switch>
 )
