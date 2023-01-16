5b373e692ec9647334610aadf004d527c55ce95d
youssef910 <youssef.sadek910@gmail.com>
2023-01-16 21:00:52 +0000

    undo changes done to employer list

--- src/Components/forms/inputs/EmployerDropDown.test.js
+++ src/Components/forms/inputs/EmployerDropDown.test.js
@@ -1,15 +1,39 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import exampleData from '../data.json'
 import EmployerDropDown from './EmployerDropDown'
-
+const employerList = [
+  { name: 'BBC', _id: 'BBC' },
+  { name: 'Bet365', _id: 'Bet365' },
+  { name: 'Blue Frontier', _id: 'Blue Frontier' },
+  { name: 'EngineerBetter', _id: 'EngineerBetter' },
+  { name: 'Brandwatch', _id: 'Brandwatch' },
+  { name: 'Equal Experts', _id: 'Equal Experts' },
+  { name: 'Infobip', _id: 'Infobip' },
+  { name: 'Synaptik Digital', _id: 'Synaptik Digital' },
+  { name: 'Tata Consultancy services', _id: 'Tata Consultancy services' },
+  { name: 'Tamarix Tech', _id: 'Tamarix Tech' },
+  { name: 'Inspired Gaming Group', _id: 'Inspired Gaming Group' },
+  { name: 'EPAM System', _id: 'EPAM System' },
+  { name: 'Blueprint Gaming', _id: 'Blueprint Gaming' },
+  { name: 'Beamery', _id: 'Beamery' },
+  { name: 'American Express ', _id: 'American Express ' },
+  { name: 'Capgemini', _id: 'Capgemini' },
+  {
+    name: 'Amazon Web Services (AWS)',
+    _id: 'Amazon Web Services (AWS)'
+  },
+  { name: 'Alpha FX Group', _id: 'Alpha FX Group' },
+  { name: 'Amazon', _id: 'Amazon' },
+  { name: 'Anaplan', _id: 'Anaplan' },
+  { name: 'Alfa Financial Software', _id: 'Alfa Financial Software' }
+]
 describe('EmployerDropDown', () => {
   it('shows a matching option for specific word', async () => {
     const user = userEvent.setup()
     render(
       <EmployerDropDown
-        arrayList={exampleData.employerList}
+        arrayList={employerList}
         employerOnChange={() => {}}
         isEmpty={false}
       />
@@ -28,7 +52,7 @@ describe('EmployerDropDown', () => {
     const user = userEvent.setup()
     render(
       <EmployerDropDown
-        arrayList={exampleData.employerList}
+        arrayList={employerList}
         employerOnChange={() => {}}
         isEmpty={false}
       />
