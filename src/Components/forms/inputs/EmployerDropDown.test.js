5785b9c98f62d23d0861ad6c0d7bcb2ca7729d7a
youssef910 <youssef.sadek910@gmail.com>
2023-02-07 00:25:28 +0000

    fix failing wrong tests and add new tests

--- src/Components/forms/inputs/EmployerDropDown.test.js
+++ src/Components/forms/inputs/EmployerDropDown.test.js
@@ -7,105 +7,80 @@ import EmployerDropDown from './EmployerDropDown'
 
 describe('EmployerDropDown', () => {
   it('shows the label value and place holder value', async () => {
-    render(
-      <EmployerDropDown
-        arrayList={[
-          { name: 'Beamery', _id: 'Beamery' },
-          { name: 'American Express', _id: 'American Express ' },
-          { name: 'Capgemini', _id: 'Capgemini' }
-        ]}
-        onChange={() => {}}
-        isEmpty={false}
-      />
-    )
-    expect(screen.getByTestId('form-group')).toHaveTextContent(
+    renderInForm({ employers: ['Arnold Clark'] })
+    expect(screen.getByTestId('form')).toHaveTextContent(
       'Who is your employer? *'
     )
-    expect(screen.getByTestId('form-group')).toHaveTextContent(
+    expect(screen.getByTestId('form')).toHaveTextContent(
       'Type your employer name here'
     )
   })
-  it('shows the selected value', async () => {
-    render(
-      <EmployerDropDown
-        arrayList={[
-          { name: 'Beamery', _id: 'Beamery' },
-          { name: 'American Express ', _id: 'American Express ' },
-          { name: 'Capgemini', _id: 'Capgemini' },
-          { name: 'Carnall Farrar', _id: 'Carnall Farrar' }
-        ]}
-        onChange={() => {}}
-        isEmpty={false}
-      />
-    )
-
+  it('renders an input', async () => {
+    renderInForm({ employers: ['Arnold Clark', 'Bankifi', 'Capgemini'] })
     await selectEvent.select(
-      screen.getByLabelText(/Who is your employer/i),
+      screen.getByLabelText(/who is your employer/i),
       'Capgemini'
     )
-    expect(screen.getByTestId('form-group')).toHaveTextContent('Capgemini')
-    expect(screen.getByTestId('form-group')).not.toHaveTextContent(
-      'Carnall Farrar'
-    )
+    expect(screen.getByTestId('form')).toHaveFormValues({
+      employer: 'Capgemini'
+    })
   })
-  it('shows the Options when typing', async () => {
-    const user = userEvent.setup()
-    render(
-      <EmployerDropDown
-        arrayList={[
-          { name: 'Beamery', _id: 'Beamery' },
-          { name: 'American Express ', _id: 'American Express ' },
-          { name: 'Capgemini', _id: 'Capgemini' },
-          { name: 'Capital', _id: 'Capital' }
-        ]}
-        onChange={() => {}}
-        isEmpty={false}
-      />,
-      { hidden: true }
+
+  it('updates the rest of the form', async () => {
+    const onChange = jest.fn()
+    renderInForm({ employers: ['Deloitte', 'EDF', 'FlexJobs'], onChange })
+    await selectEvent.select(
+      screen.getByLabelText(/who is your employer/i),
+      'EDF'
     )
-    await user.type(screen.getByRole('combobox', { id: 'employer' }), 'cap')
-    expect(screen.getByTestId('form-group')).toHaveTextContent('Capgemini')
-    expect(screen.getByTestId('form-group')).toHaveTextContent('Capital')
-    expect(screen.getByTestId('form-group')).not.toHaveTextContent('Beamery')
+    expect(onChange).toHaveBeenCalledWith({
+      target: expect.objectContaining({
+        name: 'employer',
+        type: 'text',
+        value: 'EDF'
+      })
+    })
   })
-  it('finds characters within words', async () => {
-    const user = userEvent.setup()
-    render(
-      <EmployerDropDown
-        arrayList={[
-          { name: 'BBC', _id: 'BBC' },
-          { name: 'Bet365', _id: 'Bet365' },
-          { name: 'Blue Frontier', _id: 'Blue Frontier' },
-          { name: 'EngineerBetter', _id: 'EngineerBetter' },
-          { name: 'Brandwatch', _id: 'Brandwatch' },
-          { name: 'Equal Experts', _id: 'Equal Experts' },
-          { name: 'Infobip', _id: 'Infobip' },
-          { name: 'Synaptik Digital', _id: 'Synaptik Digital' },
-          {
-            name: 'Tata Consultancy services',
-            _id: 'Tata Consultancy services'
-          },
-          { name: 'Tamarix Tech', _id: 'Tamarix Tech' },
-          { name: 'Inspired Gaming Group', _id: 'Inspired Gaming Group' },
-          { name: 'EPAM System', _id: 'EPAM System' },
-          { name: 'Blueprint Gaming', _id: 'Blueprint Gaming' },
-          { name: 'Beamery', _id: 'Beamery' },
-          { name: 'American Express ', _id: 'American Express ' },
-          { name: 'Capgemini', _id: 'Capgemini' },
-          {
-            name: 'Amazon Web Services (AWS)',
-            _id: 'Amazon Web Services (AWS)'
-          },
-          { name: 'Alpha FX Group', _id: 'Alpha FX Group' },
-          { name: 'Amazon', _id: 'Amazon' },
-          { name: 'Anaplan', _id: 'Anaplan' },
-          { name: 'Alfa Financial Software', _id: 'Alfa Financial Software' }
-        ]}
-        employerOnChange={() => {}}
-        isEmpty={false}
-      />,
-      { hidden: true }
-    )
+
+  it('applies the right class when empty', async () => {
+    const { container } = renderInForm({ isEmpty: true })
+    expect(container.getElementsByClassName('is-empty')).toHaveLength(1)
+  })
+
+  it('asks user to select "Other" if no matches', async () => {
+    const { user } = renderInForm({ employers: ['G-Research'] })
+    await user.type(screen.getByRole('combobox', { name: /employer/i }), 'cap')
+    expect(
+      screen.getByText('Employer not found? Please select "Other".')
+    ).toBeInTheDocument()
+  })
+
+  it('shows the expected values in AC', async () => {
+    const { user } = renderInForm({
+      employers: [
+        'BBC',
+        'Bet365',
+        'Blue Frontier',
+        'EngineerBetter',
+        'Brandwatch',
+        'Equal Experts',
+        'Infobip',
+        'Synaptik Digital',
+        'Tata Consultancy services',
+        'Tamarix Tech',
+        'Inspired Gaming Group',
+        'EPAM System',
+        'Blueprint Gaming',
+        'Beamery',
+        'American Express ',
+        'Capgemini',
+        'Amazon Web Services (AWS)',
+        'Alpha FX Group',
+        'Amazon',
+        'Anaplan'
+      ]
+    })
+    await user.type(screen.getByRole('combobox', { name: /employer/i }), 'am')
     const appearingEmployers = [
       'Amazon',
       'Amazon Web Services (AWS)',
@@ -116,27 +91,27 @@ describe('EmployerDropDown', () => {
       'Inspired Gaming Group',
       'Tamarix Tech'
     ]
-    const notAppearingApplicants = [
-      'Alpha FX Group',
-      'Anaplan',
-      'BBC',
-      'Bet365',
-      'Brandwatch',
-      'Blue Frontier',
-      'EngineerBetter',
-      'Equal Experts',
-      'Infobip',
-      'Tata Consultancy services',
-      'Synaptik Digital'
-    ]
-
-    await user.type(screen.getByRole('combobox', { id: 'employer' }), 'am')
 
     appearingEmployers.forEach(employer =>
-      expect(screen.getByTestId('form-group')).toHaveTextContent(employer)
-    )
-    notAppearingApplicants.forEach(employer =>
-      expect(screen.getByTestId('form-group')).not.toHaveTextContent(employer)
+      expect(screen.getByText(employer)).toBeInTheDocument()
     )
   })
 })
+const renderInForm = ({
+  employers = [],
+  isEmpty = false,
+  onChange = () => {}
+}) => {
+  const user = userEvent.setup()
+  const wrapper = render(
+    <form data-testid="form">
+      <EmployerDropDown
+        arrayList={employers.map(name => ({ _id: name, name }))}
+        isEmpty={isEmpty}
+        name="employer"
+        onChange={onChange}
+      />
+    </form>
+  )
+  return { user, ...wrapper }
+}
