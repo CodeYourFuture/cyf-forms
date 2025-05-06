b4878a43af81eb919f54576fa8d6fdbd0a7919f3
Mark Farmiloe <mark@ccsdev.net>
2025-05-07 00:12:34 +0100

    Upgrade all dependencies to latest, apart from react, react-dom to v18

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { applyMiddleware, compose, createStore } from 'redux'
-import ReduxThunk from 'redux-thunk'
+import { thunk } from 'redux-thunk'
 import './App.css'
 import Footer from './Components/Footer'
 import Navbar from './Components/Navbar'
@@ -12,7 +12,7 @@ import Routes from './Routes'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   reducers,
-  composeEnhancers(applyMiddleware(ReduxThunk))
+  composeEnhancers(applyMiddleware(thunk))
 )
 
 const App = () => (
