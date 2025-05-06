c2dafee280d260ccef9e5706d633710c15d198a1
Mark Farmiloe <mark@ccsdev.net>
2025-05-07 00:52:02 +0100

    Move to configureStore

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { applyMiddleware, compose, createStore } from 'redux'
-import { thunk } from 'redux-thunk'
+import { configureStore } from '@reduxjs/toolkit'
 import './App.css'
 import Footer from './Components/Footer'
 import Navbar from './Components/Navbar'
 import reducers from './Redux/Reducer'
 import Routes from './Routes'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(
-  reducers,
-  composeEnhancers(applyMiddleware(thunk))
-)
+const store = configureStore({
+  reducer: reducers
+})
 
 const App = () => (
   <Provider store={store}>
